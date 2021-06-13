c41df068bb6cb80aab228328b110f297341c21a9
Alonso202 <drosk1711@gmail.com>
2021-06-13 00:32:32 -0400

    Se a agregado vistas,urls estruc. django

---
+++ Exp3_backend_PerezEspina_002D/core/views.py
@@ -0,0 +1,6 @@
+from django.shortcuts import render
+# Create your views here.
+def index(request):
+    return render(request,'index.html')
+def galeria(request):
+    return render(request,'Galeria.html')
