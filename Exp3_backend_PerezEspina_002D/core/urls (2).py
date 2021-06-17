bcf04a7c86c070d10990d53d287c416c5e348548
Alonso Perez <Alonso202>
2021-06-17 14:18:25 -0400

    se ha eliminado archivos duplicados

--- Exp3_backend_PerezEspina_002D/core/urls (2).py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from .views import index,galeria,login_registro
-
-
-urlpatterns = [
-    path ('', index, name="index"),
-    path('galeria',galeria, name="galeria"),
-    path('login_registro',login_registro, name="login_registro")
-]
